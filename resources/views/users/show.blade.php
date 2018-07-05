056affa7d741e8ade471a5da8652c5e321fe1526
EC2 Default User <aajiro.kirameki@techacademy.jp>
2018-07-05 06:18:17 +0000

    login

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,33 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>[Thu Jul  5 06:12:31 2018] 127.0.0.1:47976 [200]: /favicon.ico
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="col-lg-6 col-sm-6">
+    <div class="card hovercard">
+        <div class="card-background">
+            <img class="card-bkimg" alt="" src="http://lorempixel.com/100/100/people/9/">
+            
+        </div>
+        <div class="useravatar">
+            <img alt="山口蛍" src="https://kruf468c.user.webaccel.jp/files/topics/19559_ext_04_0.jpg?v=1476171045">
+        </div>
+        <div class="card-info"> <span class="card-title">山口蛍</span>
+        </div>
+    </div>
+  
+    
+    <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
+        <div class="btn-group" role="group">
+            <button type="button" id="follow" class="btn btn-primary" href="#tab1" data-toggle="tab"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <div class="hidden-xs">フォロー</div>
+            </button>
+        </div>
+        <div class="btn-group" role="group">
+            <button type="button" id="followers" class="btn btn-default" href="#tab3" data-toggle="tab"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                <div class="hidden-xs">フォロワー</div>
+            </button>
+        </div>
+    </div>
+
+</div>
